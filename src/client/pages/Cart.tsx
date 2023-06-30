a6eed910ed15b421bf3cd50aef61618228659b41
Sobolevskiy Vitaliy <SobVitalya@yandex.ru>
2023-06-30 18:10:05 +0700

    feat: add data-testid cart page

--- src/client/pages/Cart.tsx
+++ src/client/pages/Cart.tsx
@@ -11,112 +11,128 @@ import { ApplicationState, checkout, clearCart } from '../store';
 const bem = cn('Cart');
 
 export const Cart: React.FC = () => {
-    const dispatch = useDispatch();
-    const cart = useSelector((s: ApplicationState) => s.cart);
-    const latestOrderId = useSelector((s: ApplicationState) => s.latestOrderId);
+  const dispatch = useDispatch();
+  const cart = useSelector((s: ApplicationState) => s.cart);
+  const latestOrderId = useSelector((s: ApplicationState) => s.latestOrderId);
 
-    const onClear = useCallback(() => {
-        dispatch(clearCart());
-    }, [dispatch]);
+  const onClear = useCallback(() => {
+    dispatch(clearCart());
+  }, [dispatch]);
 
-    const onSubmit = useCallback((form: CheckoutFormData) => {
-        dispatch(checkout(form, cart));
-    }, [dispatch, cart]);
+  const onSubmit = useCallback(
+    (form: CheckoutFormData) => {
+      dispatch(checkout(form, cart));
+    },
+    [dispatch, cart],
+  );
 
-    let content: React.ReactNode = null;
+  let content: React.ReactNode = null;
 
-    const cartIsEmpty = !Object.keys(cart).length;
+  const cartIsEmpty = !Object.keys(cart).length;
 
-    if (!cartIsEmpty) {
-        const rows = Object.entries(cart).map(([id, item], index) => {
+  if (!cartIsEmpty) {
+    const rows = Object.entries(cart).map(([id, item], index) => {
+      return (
+        <tr key={id} data-testid={id}>
+          <th className={bem('Index')} scope='row'>
+            {index + 1}
+          </th>
+          <td className={bem('Name')}>{item.name}</td>
+          <td className={bem('Price')}>${item.price}</td>
+          <td className={bem('Count')}>{item.count}</td>
+          <td className={bem('Total')}>${item.count * item.price}</td>
+        </tr>
+      );
+    });
 
-            return (
-                <tr key={id} data-testid={id}>
-                    <th className={bem('Index')} scope="row">{index + 1}</th>
-                    <td className={bem('Name')}>{item.name}</td>
-                    <td className={bem('Price')}>${item.price}</td>
-                    <td className={bem('Count')}>{item.count}</td>
-                    <td className={bem('Total')}>${item.count * item.price}</td>
-                </tr>
-            );
-        });
+    const total = Object.values(cart).reduce((sum, { count, price }) => sum + count * price, 0);
 
-        const total = Object.values(cart).reduce((sum, { count, price }) => sum + count * price, 0);
-
-        content = (
-            <table className={bem('Table', ['table'])}>
-                <thead>
-                    <tr>
-                        <th scope="col">#</th>
-                        <th scope="col">Product</th>
-                        <th scope="col">Price</th>
-                        <th scope="col">Count</th>
-                        <th scope="col">Total</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {rows}
-                </tbody>
-                <tfoot>
-                    <tr>
-                        <td colSpan={4}>Order price:</td>
-                        <td className={bem('OrderPrice')}>${total}</td>
-                    </tr>
-                </tfoot>
-            </table>
-        );
-    } else {
-        content = (
-            <>
-                Cart is empty. Please select products in the <Link to="/catalog">catalog</Link>.
-            </>
-        );
-    }
-
-    const actions = cartIsEmpty ? null : (
-        <div className="row mb-4">
-            <div className="col-6">
-                <button className={bem('Clear', ['btn', 'btn-outline-secondary'])} onClick={onClear}>Clear shopping cart</button>
-            </div>
-        </div>
+    content = (
+      <table data-testid='table' className={bem('Table', ['table'])}>
+        <thead>
+          <tr>
+            <th scope='col'>#</th>
+            <th scope='col'>Product</th>
+            <th scope='col'>Price</th>
+            <th scope='col'>Count</th>
+            <th scope='col'>Total</th>
+          </tr>
+        </thead>
+        <tbody>{rows}</tbody>
+        <tfoot>
+          <tr>
+            <td colSpan={4}>Order price:</td>
+            <td className={bem('OrderPrice')}>${total}</td>
+          </tr>
+        </tfoot>
+      </table>
     );
-
-    const form = cartIsEmpty ? null : (
-        <div className="row">
-            <div className="col-12 col-sm-8 col-md-6 col-lg-4">
-                <h2>Сheckout</h2>
-                <Form onSubmit={onSubmit} />
-            </div>
-        </div>
+  } else {
+    content = (
+      <>
+        Cart is empty. Please select products in the{' '}
+        <Link data-testid='emptyLink' to='/catalog'>
+          catalog
+        </Link>
+        .
+      </>
     );
+  }
 
-    const alertClass = process.env.BUG_ID !== '8' ? 'alert-success' : 'alert-danger';
+  const actions = cartIsEmpty ? null : (
+    <div className='row mb-4'>
+      <div className='col-6'>
+        <button
+          data-testid='clear'
+          className={bem('Clear', ['btn', 'btn-outline-secondary'])}
+          onClick={onClear}
+        >
+          Clear shopping cart
+        </button>
+      </div>
+    </div>
+  );
 
-    const orderInfo = cartIsEmpty && latestOrderId ? (
-        <div className="row my-2">
-            <div className="col-12 col-sm-8 col-md-6">
-                <div className={bem('SuccessMessage', ['alert', alertClass])}>
-                    <h4 className="alert-heading">Well done!</h4>
-                    <p>Order #<strong className={bem('Number')}>{latestOrderId}</strong> has been successfully completed.</p>
-                    <hr/>
-                    <p className="mb-0">Please wait for confirmation of delivery.</p>
-                </div>
-            </div>
+  const form = cartIsEmpty ? null : (
+    <div className='row'>
+      <div className='col-12 col-sm-8 col-md-6 col-lg-4'>
+        <h2>Сheckout</h2>
+        <Form onSubmit={onSubmit} />
+      </div>
+    </div>
+  );
+
+  const alertClass = process.env.BUG_ID !== '8' ? 'alert-success' : 'alert-danger';
+
+  const orderInfo =
+    cartIsEmpty && latestOrderId ? (
+      <div className='row my-2'>
+        <div className='col-12 col-sm-8 col-md-6'>
+          <div className={bem('SuccessMessage', ['alert', alertClass])}>
+            <h4 className='alert-heading'>Well done!</h4>
+            <p>
+              Order #<strong className={bem('Number')}>{latestOrderId}</strong> has been successfully
+              completed.
+            </p>
+            <hr />
+            <p className='mb-0'>Please wait for confirmation of delivery.</p>
+          </div>
         </div>
+      </div>
     ) : null;
 
-    return (
-        <div className={bem()}>
-            <Helmet title="Shopping cart" />
-            <div className="row mb-4">
-                <div className="col">
-                    <h1>Shopping cart</h1>
-                    {orderInfo}
-                    {content}
-                </div>
-            </div>
-            {actions}
-            {form}
+  return (
+    <div className={bem()}>
+      <Helmet title='Shopping cart' />
+      <div className='row mb-4'>
+        <div className='col'>
+          <h1>Shopping cart</h1>
+          {orderInfo}
+          {content}
         </div>
-    );
-}
+      </div>
+      {actions}
+      {form}
+    </div>
+  );
+};
